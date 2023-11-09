2778c8278ba0d29d6195347fc415043d9d63cb38
darcklito <luis_paulino5@ucne.edu.do>
2023-11-08 23:14:29 -0400

    modelo de entrada junto a su detalle creado

---
+++ LuisJose_AP1_P2/Shared/Entradas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisJose_AP1_P2.Shared
+{
+	internal class Entradas
+	{
+		[Key]
+		public int EntradaId { get; set; }
+		public DateTime Fecha { get; set; } = DateTime.Now;
+
+		[Required(ErrorMessage = "El Concepto es un campo requerido")]
+		public string? Concepto { get; set; }
+
+		[Required(ErrorMessage = "El ProductoID es un campo requerido")]
+		public int ProductoId { get; set; }
+
+		[Required(ErrorMessage = "Se requiere especificar la cantidad producida")]
+		public int CantidadProducida { get; set; }
+
+		[ForeignKey("EntradaId")]
+		public List<EntradasDetalle> entradasDetalle { get; set; } = new List<EntradasDetalle>();
+	}
+}
---
+++ LuisJose_AP1_P2/Shared/EntradasDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisJose_AP1_P2.Shared
+{
+	internal class EntradasDetalle
+	{
+		[Key]
+		public int DetalleID { get; set; }
+		public int EntradaID { get; set; }
+
+		[Required(ErrorMessage = "El ProductoID es un campo requerido")]
+		public int ProductoID { get; set; }
+
+		[Required(ErrorMessage = "Se requiere especificar la cantidad utilizada")]
+		public int CantidadUtilizada { get; set; }
+	}
+}
